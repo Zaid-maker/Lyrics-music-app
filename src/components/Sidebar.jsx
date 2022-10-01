591400af895b835a195b679b0565ae1bb3787339
Zaid-maker <pzhafeez@gmail.com>
2022-10-01 14:42:46 +0500

    f2

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -46,13 +46,23 @@ const Sidebar = () => {
 
       <div className="absolute md:hidden block top-6 right-3">
         {!mobileMenuOpen ? (
-          <HiOutlineMenu className="w-6 h-6 mr-6 text-white" onClick={() => setMobileMenuOpen(true)} />
+          <HiOutlineMenu
+            className="w-6 h-6 mr-2 text-white"
+            onClick={() => setMobileMenuOpen(true)}
+          />
         ) : (
-          <RiCloseLine className="w-6 h-6 mr-2 text-white" onClick={() => setMobileMenuOpen(false)} />
+          <RiCloseLine
+            className="w-6 h-6 mr-2 text-white"
+            onClick={() => setMobileMenuOpen(false)}
+          />
         )}
       </div>
 
-      <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#483D8B] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${mobileMenuOpen ? 'left-0' : '-left-full'}`}>
+      <div
+        className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#483D8B] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${
+          mobileMenuOpen ? 'left-0' : '-left-full'
+        }`}
+      >
         <img src={logo} alt="logo" className="w-full h-14 object-contain" />
         <NavLinks handleClick={() => setMobileMenuOpen(false)} />
       </div>
