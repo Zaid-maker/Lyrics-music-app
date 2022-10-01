bf3aa54303a17240e825228aa73a531365c540ba
Zaid-maker <pzhafeez@gmail.com>
2022-10-01 22:41:49 +0500

    Song card sheesh

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -1,5 +1,29 @@
-const SongCard = () => (
-  <div>SongCard</div>
+import { Link } from 'react-router-dom';
+
+import PlayPause from './PlayPause';
+
+const SongCard = ({ song, isPlaying, activeSong, data, i }) => (
+  <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
+    <div className="relative w-full h-56 group">
+      <div className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${activeSong?.title === song.title ? 'flex bg-black bg-opacity-70' : 'hidden'}`}>
+        <PlayPause isPlaying={isPlaying} activeSong={activeSong} song={song} />
+      </div>
+      <img src={song.images?.coverart} alt="song_img" className="w-full h-full rounded-lg" />
+    </div>
+
+    <div className="mt-4 flex flex-col">
+      <p className="font-semibold text-lg text-white truncate">
+        <Link to={`/songs/${song?.key}`}>
+          {song.title}
+        </Link>
+      </p>
+      <p className="text-sm truncate text-gray-300 mt-1">
+        <Link to={song.artists ? `/artists/${song?.artists[0]?.adamid}` : '/top-artists'}>
+          {song.subtitle}
+        </Link>
+      </p>
+    </div>
+  </div>
 );
 
 export default SongCard;
