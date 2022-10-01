dd03057c3555b9fd78a9e726faf9ef5f3c4478fd
Zaid-maker <pzhafeez@gmail.com>
2022-10-01 23:14:03 +0500

    just for fun things xD

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
-const CountryTracks = () => <div>CountryTracks</div>;
+const CountryTracks = () => (
+  <div className="flex flex-col">
+    <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">Around You <span className="font-black">Pakistan</span></h2>
+  </div>
+);
 
 export default CountryTracks;
--- src/pages/TopArtists.jsx
+++ src/pages/TopArtists.jsx
@@ -1,3 +1,7 @@
-const TopArtists = () => <div>TopArtists</div>;
+const TopArtists = () => (
+  <div className="flex flex-col">
+    <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">Discover Top Artists</h2>
+  </div>
+);
 
 export default TopArtists;
