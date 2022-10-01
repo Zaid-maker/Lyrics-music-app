3c4764b1af02e141365ef5cc271396c3543bb029
Zaid-maker <pzhafeez@gmail.com>
2022-10-01 14:07:31 +0500

    what i did wrong ufff

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -17,16 +17,21 @@ const links = [
   { name: 'Top Charts', to: '/top-charts', icon: HiOutlineHashtag },
 ];
 
-const NavLinks = () => {
+const NavLinks = ({ handleClick }) => (
   <div className="mt-10">
     {links.map((item) => (
-      <NavLink key={item.name} to={item.to} className="flex flex-row justify-start items-center my-8 text-sm font-medium text-gray-400 hover:text-cyan-400">
+      <NavLink
+        key={item.name}
+        to={item.to}
+        className="flex flex-row justify-start items-center my-8 text-sm font-medium text-gray-400 hover:text-cyan-400"
+        onClick={() => handleClick && handleClick()}
+      >
         <item.icon className="w-6 h-6 mr-2" />
         {item.name}
       </NavLink>
     ))}
-  </div>;
-};
+  </div>
+);
 
 const Sidebar = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
