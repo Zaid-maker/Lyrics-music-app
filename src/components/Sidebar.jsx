b1509a6d8a0194d27b59e19c10b11b9157d5bce5
Zaid-maker <pzhafeez@gmail.com>
2022-10-01 14:41:09 +0500

    f

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,6 +6,7 @@ import {
   HiOutlinePhotograph,
   HiOutlineUserGroup,
 } from 'react-icons/hi';
+import { RiCloseLine } from 'react-icons/ri';
 import { NavLink } from 'react-router-dom';
 
 import { logo } from '../assets';
@@ -37,10 +38,25 @@ const Sidebar = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
-      <img src={logo} alt="logo" className="w-full h-14 object-contain" />
-      <NavLinks />
-    </div>
+    <>
+      <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
+        <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+        <NavLinks />
+      </div>
+
+      <div className="absolute md:hidden block top-6 right-3">
+        {!mobileMenuOpen ? (
+          <HiOutlineMenu className="w-6 h-6 mr-6 text-white" onClick={() => setMobileMenuOpen(true)} />
+        ) : (
+          <RiCloseLine className="w-6 h-6 mr-2 text-white" onClick={() => setMobileMenuOpen(false)} />
+        )}
+      </div>
+
+      <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#483D8B] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${mobileMenuOpen ? 'left-0' : '-left-full'}`}>
+        <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+        <NavLinks handleClick={() => setMobileMenuOpen(false)} />
+      </div>
+    </>
   );
 };
 
