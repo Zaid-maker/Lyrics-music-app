f9da296ff1c188e366f7cbb906fcebc2470ad0b7
Zaid-maker <pzhafeez@gmail.com>
2022-10-03 22:27:13 +0500

    Artist detail fixes

--- src/pages/ArtistDetails.jsx
+++ src/pages/ArtistDetails.jsx
@@ -7,7 +7,7 @@ import { useGetArtistDetailsQuery } from '../redux/services/shazamCore';
 const ArtistDetails = () => {
   const { id: artistId } = useParams();
   const { activeSong, isPlaying } = useSelector((state) => state.player);
-  const { data: artistsData, isFetching: isFetchingArtistDetails, error } = useGetArtistDetailsQuery();
+  const { data: artistData, isFetching: isFetchingArtistDetails, error } = useGetArtistDetailsQuery(artistId);
 
   if (isFetchingArtistDetails) return <Loader title="Loading artist details..." />;
 
@@ -17,11 +17,11 @@ const ArtistDetails = () => {
     <div className="flex flex-col">
       <DetailsHeader
         artistId={artistId}
-        artistData={artistsData}
+        artistData={artistData}
       />
 
       <RelatedSongs
-        data={Object.values(artistsData?.songs)}
+        data={Object.values(artistData?.songs)}
         artistId={artistId}
         isPlaying={isPlaying}
         activeSong={activeSong}
