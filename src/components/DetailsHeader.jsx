98a1669f2f54bbd19839758433c9a26568a8eb53
Zaid-maker <pzhafeez@gmail.com>
2022-10-03 21:19:49 +0500

    Artist detail hmmmm

--- src/components/DetailsHeader.jsx
+++ src/components/DetailsHeader.jsx
@@ -1,5 +1,20 @@
-const DetailsHeader = () => (
-  <div>DetailsHeader</div>
+const DetailsHeader = ({ artistId, artistData, songData }) => (
+  <div className="relative w-full flex flex-col">
+    <div className="w-full bg-gradient-to-l from-transparent to-black sm:h-48 h-28" />
+
+    <div>
+      <img
+        src={
+          artistId
+            ? artistData?.articles[artistId].attributes?.artwork?.url
+              .replace('{w}', '500')
+              .replace('{h}', '500')
+            : songData?.images?.coverart
+        }
+        alt="profile"
+      />
+    </div>
+  </div>
 );
 
 export default DetailsHeader;
--- src/pages/ArtistDetails.jsx
+++ src/pages/ArtistDetails.jsx
@@ -1,5 +1,33 @@
-const ArtistDetails = () => (
-  <div>ArtistDetails</div>
-);
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
+
+import { useGetArtistDetailsQuery } from '../redux/services/shazamCore';
+
+const ArtistDetails = () => {
+  const { id: artistId } = useParams();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data: artistsData, isFetching: isFetchingArtistDetails, error } = useGetArtistDetailsQuery();
+
+  if (isFetchingArtistDetails) return <Loader title="Loading artist details..." />;
+
+  if (error) return <Error />;
+
+  return (
+    <div className="flex flex-col">
+      <DetailsHeader
+        artistId={artistId}
+        artistData={artistsData}
+      />
+
+      <RelatedSongs
+        data={Object.values(artistsData?.songs)}
+        artistId={artistId}
+        isPlaying={isPlaying}
+        activeSong={activeSong}
+      />
+    </div>
+  );
+};
 
 export default ArtistDetails;
