5a496ab94bde347583612c38887e60c87cb17dfe
Zaid-maker <pzhafeez@gmail.com>
2022-10-24 21:58:13 +0500

    formats

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -14,32 +14,38 @@ const CountryTracks = () => {
 
   useEffect(() => {
     axios
-      .get(`https://geo.ipify.org/api/v2/country?apiKey=${import.meta.env.VITE_GEO_API_KEY}`)
+      .get(
+        `https://geo.ipify.org/api/v2/country?apiKey=${
+          import.meta.env.VITE_GEO_API_KEY
+        }`,
+      )
       .then((res) => setCountry(res?.data?.location.country))
       .catch((err) => console.log(err))
       .finally(() => setLoading(false));
   }, [country]);
 
-  if (isFetching && loading) return <Loader title="Loading Songs around you..." />;
+  if (isFetching && loading) { return <Loader title="Loading Songs around you..." />; }
 
   if (error && country !== '') return <Error />;
 
   return (
     <div className="flex flex-col">
       <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">
-        Around You <span className="font-black">{country}</span>
+        Around you <span className="font-black">{country}</span>
       </h2>
 
-      {data?.map((song, i) => (
-        <SongCard
-          key={song.key}
-          song={song}
-          activeSong={activeSong}
-          isPlaying={isPlaying}
-          data={data}
-          i={i}
-        />
-      ))}
+      <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+        {data?.map((song, i) => (
+          <SongCard
+            key={song.key}
+            song={song}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            data={data}
+            i={i}
+          />
+        ))}
+      </div>
     </div>
   );
 };
