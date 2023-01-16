d72b012164ba0583c01a034d9e11b45dca703030
Zaid-maker <pzhafeez@gmail.com>
2023-01-16 20:56:36 +0500

    song details

--- src/components/RelatedSongs.jsx
+++ src/components/RelatedSongs.jsx
@@ -9,7 +9,7 @@ const RelatedSongs = ({
   handlePlayClick,
 }) => (
   <div className="flex flex-col">
-    <h1 className="font-bold text-3xl text-white">Related Songs</h1>
+    <h1 className="font-bold text-3xl text-white">Related Songs:</h1>
 
     <div className="mt-6 w-full flex flex-col">
       {data?.map((song, i) => (
--- src/components/SongBar.jsx
+++ src/components/SongBar.jsx
@@ -3,7 +3,15 @@ import { Link } from 'react-router-dom';
 
 import PlayPause from './PlayPause';
 
-const SongBar = ({ song, i, artistId, isPlaying, activeSong, handlePauseClick, handlePlayClick }) => (
+const SongBar = ({
+  song,
+  i,
+  artistId,
+  isPlaying,
+  activeSong,
+  handlePauseClick,
+  handlePlayClick,
+}) => (
   <div className={`w-full flex flex-row items-center hover:bg-[#4c426e] ${activeSong?.title === song?.title ? 'bg-[#4c426e]' : 'bg-transparent'} py-2 p-4 rounded-lg cursor-pointer mb-2`}>
     <h3 className="font-bold text-base text-white mr-3">{i + 1}.</h3>
     <div className="flex-1 flex flex-row justify-between items-center">
