88fd3cf67e78c7f3cd74f70261b2eae6716d265b
Zaid-maker <pzhafeez@gmail.com>
2023-01-16 18:50:07 +0500

    fix

--- src/components/DetailsHeader.jsx
+++ src/components/DetailsHeader.jsx
@@ -6,29 +6,29 @@ const DetailsHeader = ({ artistId, artistData, songData }) => (
 
     <div className="absolute inset-0 flex items-center">
       <img
+        alt="profile"
         src={
-          artistId ? artistData?.artists[artistId].attributes?.artwork?.url
+          artistId ? artistData?.attributes?.artwork?.url
             .replace('{w}', '500')
             .replace('{h}', '500')
             : songData?.images?.coverart
-        }
-        alt="profile"
+}
         className="sm:w-48 w-28 sm:h-48 h-28 rounded-full object-cover border-2 shadow-xl shadow-black"
       />
 
       <div className="ml-5">
         <p className="font-bold sm:text-3xl text-xl text-white">
-          {artistId ? artistData?.artists[artistId].attributes?.name : songData?.title}
+          {artistId ? artistData?.attributes?.name : songData?.title}
         </p>
         {!artistId && (
           <Link to={`/artists/${songData?.artists[0]?.adamid}`}>
-            <p>{songData?.subtitle}</p>
+            <p className="text-base text-gray-400 mt-2">{songData?.subtitle}</p>
           </Link>
         )}
 
         <p className="text-base text-gray-400 mt-2">
           {artistId
-            ? artistData?.artists[artistId].attributes?.genreNames[0]
+            ? artistData?.attributes?.genreNames[0]
             : songData?.genres?.primary}
         </p>
       </div>
